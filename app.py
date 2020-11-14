31ecb75aeb9c6c5e5dec6bcc3dc2a6e7dd8dbefe
Velidonimo <velidon@mail.ru>
2020-11-14 23:01:50 +0300

    Table in PostgreSQL created

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import request, render_template, Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:123@localhost/height_collector'
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    height = db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self.email = email
+        self.height = height
 
 
 @app.route('/')
@@ -13,8 +28,8 @@ def success():
     if request.method == "POST":
         email = request.form['email_name']
         height = int(request.form['height_name'])
-    print(email, height)
-    return render_template("success.html")
+        print(email, height)
+        return render_template("success.html")
 
 
 if __name__ == '__main__':
